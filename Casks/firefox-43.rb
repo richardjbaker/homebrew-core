06a12e523ab466e7813677d914ee99950e1685a0
Richard Baker <baker.richard@gmail.com>
2019-03-28 05:39:01 +0000

    Create firefox-43.rb

---
+++ Casks/firefox-43.rb
@@ -0,0 +1,24 @@
+cask 'firefox' do
+  version '43.0.1'
+  sha256 '4e14ecf74467f5227a1c15e56705b4b954a90d443dfba696f9dd908478e39dea'
+
+  url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
+  name 'Firefox'
+  name 'Mozilla Firefox'
+  homepage 'https://www.mozilla.org/en-US/firefox/'
+  license :mpl
+  tags :vendor => 'Mozilla'
+
+  app 'Firefox.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Firefox',
+                  '~/Library/Caches/Firefox',
+                 ]
+
+  caveats <<-EOS.undent
+  The Mac App Store version of 1Password won't work with a Homebrew-cask-linked Mozilla Firefox. To bypass this limitation, you need to either:
+    + Move Mozilla Firefox to your /Applications directory (the app itself, not a symlink).
+    + Install 1Password from outside the Mac App Store (licenses should transfer automatically, but you should contact AgileBits about it).
+  EOS
+end
