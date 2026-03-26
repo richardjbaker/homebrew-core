d48e674c686e80090d07d819688f033badfaf4e5
Richard Baker <baker.richard@gmail.com>
2026-03-26 12:03:14 +0000

    Update chromedriver.rb to 146

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,9 +1,9 @@
 cask "chromedriver" do
   arch arm: "arm64", intel: "x64"
 
-  version "144.0.7559.109"
-  sha256 arm:   "2f3f3b09dca705f3fbf4e194020a53cd785de80fa0ed8ec422d1b43e911ce92e",
-         intel: "ce402dc6b7b35bc28210a677714953f05dcb2dafd03cce0a613750158336a7d3"
+  version "146.0.7680.165"
+  sha256 arm:   "43115f874f6590de5525eadf1eb441a02cb1cb907f2a3973f820ca808425d8d6",
+         intel: "691fa9d77ec37cf6f7b0d10331dbe433a41e9f7fad810996956423dd820369e3"
 
   url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chromedriver-mac-#{arch}.zip",
       verified: "storage.googleapis.com/chrome-for-testing-public/"
